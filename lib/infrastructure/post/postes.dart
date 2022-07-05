e73aefb13022e6e4adca717caba35aeb45198737
Rahulraj <rahulrohiniraju@gmail.com>
2022-07-05 11:08:50 +0530

    post func

--- lib/infrastructure/post/postes.dart
+++ lib/infrastructure/post/postes.dart
@@ -1,51 +1,106 @@
-// import 'dart:convert';
-// import 'dart:developer';
+import 'dart:convert';
+import 'dart:developer';
 
-// import 'package:dio/dio.dart';
-// import 'package:open_box/data/models/post/m_post.dart';
+import 'package:dio/dio.dart';
+import 'package:open_box/data/models/post/m_post.dart';
 
-// class Post {
-//     final dio = Dio(BaseOptions(baseUrl: 'localhost:5000'));
+class PostFunc {
+  static const postUrl = "http://localhost:5000/post";
+  final dio = Dio(BaseOptions(baseUrl: 'localhost:5000'));
+  Map<String, String> requestHeaders = {
+    'Content-Type': 'application/json',
+    'Authorization': ''
+  };
 
-//   Future<List<Post?>> getTimeLinePost(String id) async {
-//     List<Post>? res;
-//     try {
-//       final response =
-//           await dio.get('/62ac12c033cde9b7e78bb802/getTimeLinePosts');
-//        res = jsonDecode(response.data);
-//       if(response.statusCode==200||response.statusCode==201){
-//         return res!;
-//       }else{
-//         print(response.statusCode);
-//       }
-//     } catch (e) {
-//       log(e.toString());
-//     }
-//     return res!;
-//   }
+  Future createPost({required Post postData, required String id}) async {
+    final data = postData;
 
+    try {
+      Response response = await dio.post('$postUrl/$id',
+          data: jsonEncode(data), options: Options(headers: requestHeaders));
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        final retrievedPost = jsonEncode(response.data);
+        return retrievedPost;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+  }
 
+  Future updatePost({required Post postData, required String id}) async {
+    final data = postData;
 
+    try {
+      Response response = await dio.post('$postUrl/$id',
+          data: jsonEncode(data), options: Options(headers: requestHeaders));
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        final retrievedPost = jsonEncode(response.data);
+        return retrievedPost;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+  }
 
-//   Future signUp({required Post postData}) async {
-//     // UserData? retrievedUser;
-//     // final data = UserSRes
-//     final data= postData;
-    
-//     try {
-//       Response response = await dio.post(
-//         'http://localhost:5000/auth/register',
-//         data: data,
-//       );
-//       if (response.statusCode == 200 || response.statusCode == 201) {
-//       final  retrievedPost = jsonEncode(response.data);
-//         print('user registered');
-//     return retrievedPost;
+  Future deletePost({required String id}) async {
+    // final data = postData;
 
-//       }
-//     } catch (e) {
-//       log(e.toString());
-//     }
-//   }
+    try {
+      Response response = await dio.post('$postUrl/$id',
+          options: Options(headers: requestHeaders));
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        final retrievedPost = jsonEncode(response.data);
+        return retrievedPost;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+  }
 
-// }
+  Future likePost({required String id}) async {
+    try {
+      Response response = await dio.post('$postUrl/$id',
+          options: Options(headers: requestHeaders));
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        final retrievedPost = jsonEncode(response.data);
+        return retrievedPost;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+
+  Future<List<Post?>> getTimeLinePost(String id) async {
+    List<Post>? res;
+
+    try {
+      final response = await dio.get(
+          '$postUrl/62ac12c033cde9b7e78bb802/getTimeLinePosts',
+          options: Options(headers: requestHeaders));
+      res = jsonDecode(response.data);
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return res!;
+      } else {}
+    } catch (e) {
+      log(e.toString());
+    }
+    return res!;
+  }
+
+  Future<List<Post?>> getPost({required String id}) async {
+    List<Post>? res;
+
+    try {
+      final response = await dio.get(
+          '$postUrl/$id',
+          options: Options(headers: requestHeaders));
+      res = jsonDecode(response.data);
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return res!;
+      } else {}
+    } catch (e) {
+      log(e.toString());
+    }
+    return res!;
+  }
+}
